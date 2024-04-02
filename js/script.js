5572b16fdb7cf933076b8a6e57e92a6badfa33e3
Angel Rodriguez <79729254+arodriguez47-source@users.noreply.github.com>
2024-04-02 15:10:38 -0500

    script fix

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function init(){
   var button = document.getElementById('entrybutton');
 
   function showText(){
-    var text = document.getElementById('entryInput');
+    var text = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = text.value;
-    alert('Angel Rodriguez:' + text.value);
+    alert('Angel Rodriguez: ' + text.value);
   }
   button.addEventListener('click', showText);
 }
