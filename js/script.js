9281e6f1fdbc5f6807f9d30513a4d4405d3f44e4
Angel Rodriguez <79729254+arodriguez47-source@users.noreply.github.com>
2024-04-02 14:40:42 -0500

    javascript update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entrybutton');
 
-
-
-
-
-
+  function showText(){
+    var text = document.getElementById('entryInput');
+    document.getElementById('textoutput').innerHTML = text.value;
+    alert('Angel Rodriguez:' + text.value);
+  }
+  button.addEventListener('click', showText);
+}
 
 window.addEventListener('load', init);
